681996f6bb664e1fd7b69b6850b6bf1cbac27500
youjiayou <look53486@gmail.com>
2026-09-23 14:58:41 +0800

    update rwd and add tips

--- composables/CSRrequest.ts
+++ composables/CSRrequest.ts
@@ -35,6 +35,7 @@ export default {
                     const path = useRoute().path
 
                     if (path !== '/login') {
+                        console.log(123)
                         ElNotification.error({
                             title: 'Unauthorized',
                             message: 'Please log in first',
